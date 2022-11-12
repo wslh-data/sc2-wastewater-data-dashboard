17573faafadbde337f63f52cf0a27c4df08c6ddf
Adelaide Roguet <33537775+AnehEf@users.noreply.github.com>
2022-11-11 18:53:21 -0600

    Create wwTime app.R

---
+++ wwTime/app.R
@@ -0,0 +1,33 @@
+library(shiny)
+
+file_url <- "http://github.com/wslh-data/sc2-wastewater-data-dashboard/blob/main/data/DashboardData.RData?raw=true"
+
+
+
+ui <- fluidPage(
+
+  basicPage(
+    h4(
+      "Last update: ",
+      textOutput("UpdateTime", container = span)
+    )
+  )
+
+)
+
+
+
+server <- function(input, output, session) {
+    
+    reactiveGetData <- reactive({
+        load(url(file_url))
+        }) %>% bindCache(format(Sys.time(),"%Y-%m-%d"))
+  
+  
+    output$UpdateTime <- renderText({
+    format(TimeStamp)
+    })
+}
+
+
+shinyApp(ui = ui, server = server)
